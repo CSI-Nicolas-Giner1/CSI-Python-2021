81c12d04bce06f31efa63a19bd988f27b3672398
CSI-Nicolas-Giner <93525033+CSI-Nicolas-Giner1@users.noreply.github.com>
2022-01-27 11:59:29 -0400

    file deserialization

---
+++ Projects/Web-Servers/FileDeserialization.py
@@ -0,0 +1,20 @@
+import json
+import os
+from pathlib import Path
+from Nation import Nation
+
+myPath = Path(__file__).parents[0]
+myFilePath = os.path.join(myPath, 'nation.json')
+data = open(myFilePath,)
+
+data= json.load(data)
+nation:Nation = Nation(**data)
+
+print(f"id: {nation.id}")
+print(f"uid: {nation.uid}")
+print(f"nationality: {nation.nationality}")
+print(f"language: {nation.language}")
+print(f"capital: {nation.capital}")
+print(f"national sport: {nation.national_sport}")
+print(f"flag: {nation.flag}")
+print("Others expected...")
